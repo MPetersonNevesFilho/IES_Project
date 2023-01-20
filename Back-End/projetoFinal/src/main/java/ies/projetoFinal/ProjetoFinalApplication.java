ccb76b04fc2d426be052061a72f3473427e79de5
Marcus Peterson Neves Filho <mpeterson.neves@ua.pt>
2023-01-20 23:02:12 +0000

    Feat - Agora o projeto roda

---
+++ Back-End/projetoFinal/src/main/java/ies/projetoFinal/ProjetoFinalApplication.java
@@ -0,0 +1,14 @@
+package ies.projetoFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
