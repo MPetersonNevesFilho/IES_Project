3220c51461c168b5cb9bf4a67799df65bae63c57
Marcus Peterson Neves Filho <mpeterson.neves@ua.pt>
2023-01-20 17:10:20 +0000

    Upt - repo no local correto

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from './components/NavBar/NavBar.js';
+import Meetings from './components/Meetings/Meetings.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Meetings />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Meetings/Meetings.js
@@ -0,0 +1,22 @@
+
+
+
+function Meetings() {
+  return (
+    <>
+        <div>
+            <div className="titleMeetings">
+                <h1>Meetings</h1>
+            </div>
+            <div className="navbarMeetings">
+                <p>Navbar dos meetings</p>
+            </div>
+            <div className="contentMeetings">
+                <p>Conteúdo dos meetings</p>
+            </div>
+        </div>
+    </>
+  );
+}
+
+export default Meetings;
