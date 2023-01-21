9f7acecf4b798dedac5703875a89b07c47d3c3a6
Marcus Peterson Neves Filho <mpeterson.neves@ua.pt>
2023-01-21 16:26:25 +0000

    Feat-Mais detalhes xD

--- Back-End/projetoFinal/src/main/java/ies/projetoFinal/service/ContactService.java
+++ Back-End/projetoFinal/src/main/java/ies/projetoFinal/service/ContactService.java
@@ -1,5 +1,26 @@
 package ies.projetoFinal.service;
 
+import java.util.List;
+import java.util.Map;
+
+import ies.projetoFinal.model.Contact;
+
 public class ContactService {
+
+    public List<Contact> getContacts() {
+        return null;
+    }
+
+    public Map<String, String> createContact(Contact contact) {
+        return null;
+    }
+
+    public Contact getContactById(int id) {
+        return null;
+    }
+
+    public Integer deleteContactById(int id) {
+        return null;
+    }
 
 }
--- Back-End/projetoFinal/src/main/java/ies/projetoFinal/service/MeetService.java
+++ Back-End/projetoFinal/src/main/java/ies/projetoFinal/service/MeetService.java
@@ -1,5 +1,29 @@
 package ies.projetoFinal.service;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import ies.projetoFinal.model.Meet;
+
+@Service
 public class MeetService {
+
+    public List<Meet> getMeetings() {
+        return null;
+    }
+
+    public Map<String, String> createMeeting(Meet meet) {
+        return null;
+    }
+
+    public Meet getMeetingById(int id) {
+        return null;
+    }
+
+    public Integer deleteMeetingById(int id) {
+        return null;
+    }
 
 }
