6aa708cebbf58aa991e88aa4a08360b7e968330a
Marcus Peterson Neves Filho <mpeterson.neves@ua.pt>
2023-01-21 16:47:01 +0000

    Feat-Services

---
+++ Back-End/projetoFinal/src/main/java/ies/projetoFinal/Repository/MeetRepository.java
@@ -0,0 +1,26 @@
+package ies.projetoFinal.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+import ies.projetoFinal.model.Meet;
+
+public class MeetRepository {
+
+    public List<Meet> findAll() {
+        return null;
+    }
+
+    public Map<String, String> save(Meet meet) {
+        return null;
+    }
+
+    public Meet findById(int id) {
+        return null;
+    }
+
+    public Integer deleteById(int id) {
+        return null;
+    }
+
+}
--- Back-End/projetoFinal/src/main/java/ies/projetoFinal/model/Contact.java
+++ Back-End/projetoFinal/src/main/java/ies/projetoFinal/model/Contact.java
@@ -1,12 +1,13 @@
 package ies.projetoFinal.model;
 
+
 import javax.annotation.Generated;
 import javax.annotation.sql.DataSourceDefinition;
-
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.stereotype.Indexed;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
