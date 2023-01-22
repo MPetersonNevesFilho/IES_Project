9541266846912b5fa3a67902343fb84a5e1a340d
Marcus Peterson Neves Filho <mpeterson.neves@ua.pt>
2023-01-22 14:31:58 +0000

    Feat - Pirmeira versão das Meetings Rows

--- frontend/src/components/Meetings/Meetings.js
+++ frontend/src/components/Meetings/Meetings.js
@@ -1,29 +1,36 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import NavbarMeetings from './NavbarMeetings';
 import './Meetings.css';
 
 
 function Meetings() {
+
+
+
+
   return (
     <>
-        <div>
+        <div className='tudoMeetings'>
             <div className="titleMeetings">
                 <h1>Meetings</h1>
             </div>
             <div className="navbarMeetings">
-                <p>Navbar dos meetings</p>
+                <NavbarMeetings />
             </div>
             <div className="contentMeetings">
                 <div className="rowMeetings">
-                    <div>
-                        <p>13:00 - 14:00</p>
+                    <div className='rowMeetingsTime'>
+                        <h5>13:00 - 14:00</h5>
                     </div>
-                    <div>
-                        <p>Título da Reunião</p>
+                    <div className='rowMeetingsTitle'>
+                        <h4>Título da Reunião</h4>
+                        <p>Reunião Id</p>
                     </div>
-                    <div>
-                        <button>Butão1</button>
-                        <button>Butão2</button>
-                        <button>Butão3</button>
+                    <div className='rowMeetingsButtons'>
+                        <Button variant="success">Iniciar</Button>
+                        <Button variant="outline-primary">Editar</Button>
+                        <Button variant="outline-danger">Excluir</Button>
                     </div>
                 </div>
             </div>
